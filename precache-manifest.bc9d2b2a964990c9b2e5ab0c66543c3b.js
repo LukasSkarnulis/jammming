213f032ba4b1bf63155f46fd284cf1b0f372b0c6
LukasSkarnulis <luksiuslukas2@gmail.com>
2020-03-16 13:28:34 +0200

    Updates

---
+++ precache-manifest.bc9d2b2a964990c9b2e5ab0c66543c3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4566725de48e86e6f160096064348619",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "0f4b85b6acea1698d2d2",
+    "url": "/jammming/static/css/main.f0990991.chunk.css"
+  },
+  {
+    "revision": "ef6e6561928aab646401",
+    "url": "/jammming/static/js/2.656fd575.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jammming/static/js/2.656fd575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4b85b6acea1698d2d2",
+    "url": "/jammming/static/js/main.97c21ac8.chunk.js"
+  },
+  {
+    "revision": "aef624557f87c96dca64",
+    "url": "/jammming/static/js/runtime-main.2d280dd7.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
